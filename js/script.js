2f067c5906e69b3ed744a692a009e0a03999ed64
Amir <72108341+Amirali-Khamseh@users.noreply.github.com>
2023-08-24 19:37:49 +0200

    Swiper library added for movies and details page for shows

--- js/script.js
+++ js/script.js
@@ -126,7 +126,7 @@ async function movieDetails() {
       <ul class="list-group">
       ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
       </ul>
-      <a href="#" target="_blank" class="btn">Visit Movie Homepage</a>
+      <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
     </div>
   </div>
   <div class="details-bottom">
@@ -151,7 +151,7 @@ async function displayPopularShow() {
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `    
-        <a href="movie-details.html?id=${show.id}">
+        <a href="tv-details.html?id=${show.id}">
         ${show.poster_path ? `<img
             src="https://image.tmdb.org/t/p/w500${show.poster_path}"
             class="card-img-top"
@@ -173,13 +173,108 @@ async function displayPopularShow() {
   })
 }
 
+/*show details*/
+async function showDetails() {
+  const id = window.location.search.split('=')[1];
+  const show = await fetchData(`tv/${id}`);
+  console.log(show)
+  displayBackDrop('show', show.backdrop_path);
+  const div = document.createElement('div');
+  div.innerHTML = `  
+    <div class="details-top">
+    <div>
+    ${show.poster_path ? `<img
+    src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+    class="card-img-top"
+    alt="${show.name}"
+    />` : `<img
+    src="images/no-image.jpg"
+    class="card-img-top"
+    alt="${show.name}"
+    />`}
+    </div>
+    <div>
+      <h2>${show.name}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${show.vote_average.toFixed(1)}/10
+      </p>
+      <p class="text-muted">Release Date: ${show.first_air_date
+    }</p >
+      <p>
+      ${show.overview}
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+      ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+      </ul>
+      <a href="${show.homepage}" target="_blank" class="btn">Visit Show Homepage</a>
+    </div >
+  </div >
+  <div class="details-bottom">
+    <h2>Show Info</h2>
+    <ul>
+      <li><span class="text-secondary">Number of episodes :</span>  ${show.number_of_episodes}</li>
+      <li><span class="text-secondary">Last episode to air :</span>  ${show.last_episode_to_air.air_date}</li>
+      <li><span class="text-secondary">Status:</span> ${show.status} </li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">
+      ${show.production_companies.map((company) => `<li>${company.name}</li>`).join('')}
+    </div>`;
+  document.querySelector('#show-details').appendChild(div);
+}
 
+/*Display slider*/
+async function displaySlider() {
+  const { results } = await fetchData('movie/now_playing')
+  results.forEach(movie => {
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide');
+    div.innerHTML = `
+    <a href="movie-details.html?id=${movie.id}">
+      <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+    </a>
+    <h4 class="swiper-rating">
+      <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(1)} / 10
+    </h4>
+ `;
+    document.querySelector('.swiper-wrapper').appendChild(div);
+
+    initSwiper();
+  })
+}
+
+function initSwiper() {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
+}
 /*Initializing the App*/
 function initialize() {
   switch (state.currentPage) {
     case '/':
     case '/index.html':
       console.log(state.currentPage);
+      displaySlider();
       displayPopularMovie();
       break;
     case '/shows.html':
@@ -191,6 +286,7 @@ function initialize() {
       console.log(state.currentPage);
       break;
     case '/tv-details.html':
+      showDetails();
       console.log(state.currentPage);
       break;
     case '/search.html':
