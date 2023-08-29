a5063ce567c3b4214827e254b70f5d7ba445654c
Amir <72108341+Amirali-Khamseh@users.noreply.github.com>
2023-08-29 18:29:17 +0200

    sall modifications for the design done

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 /* Object holding the state of an application */
 const state = {
   currentPage: window.location.pathname,
-  searchObj:{
-    term:'',
-    type:'',
+  searchObj: {
+    term: '',
+    type: '',
     page: 1,
     totalPages: 1,
     totalResults: 0,
   },
-  api:{
-     /*For this project iam not going to use .env files to hide the API key , because i wanna use github pages */
-  APIKey : 'd926d39bce6b60805d4101875ebcfac8',
-  APIURL : 'https://api.themoviedb.org/3/',
+  api: {
+    /*For this project iam not going to use .env files to hide the API key , because i wanna use github pages */
+    APIKey: 'd926d39bce6b60805d4101875ebcfac8',
+    APIURL: 'https://api.themoviedb.org/3/',
   }
 }
 
@@ -30,7 +30,7 @@ function linkHighlight() {
 
 /*Fetching data from  API */
 async function fetchData(endPoint) {
- 
+
   const result = await fetch(`${state.api.APIURL}${endPoint}?api_key=${state.api.APIKey}&language=en-US`);
   showSpinner();
   data = result.json();
@@ -256,28 +256,28 @@ async function displaySlider() {
   })
 }
 /*Searching movies and shows */
-async function search(){
- const queryParam = window.location.search;
- const urlParam = new URLSearchParams(queryParam);
- state.searchObj.type = urlParam.get('type');
- state.searchObj.term = urlParam.get('search-term');
- if(state.searchObj.term !== '' &&state.searchObj.term !== null){
-  const {results,total_pages,page,total_results} = await searchAPI();
-state.searchObj.page = page;
-state.searchObj.totalPages = total_pages;
-state.searchObj.totalResults = total_results;
+async function search() {
+  const queryParam = window.location.search;
+  const urlParam = new URLSearchParams(queryParam);
+  state.searchObj.type = urlParam.get('type');
+  state.searchObj.term = urlParam.get('search-term');
+  if (state.searchObj.term !== '' && state.searchObj.term !== null) {
+    const { results, total_pages, page, total_results } = await searchAPI();
+    state.searchObj.page = page;
+    state.searchObj.totalPages = total_pages;
+    state.searchObj.totalResults = total_results;
 
 
-  if(results.length===0){
-    showAlert("We couldn't find this one !",'alert-error')
+    if (results.length === 0) {
+      showAlert("We couldn't find this one !", 'alert-error')
+    }
+    displaySearchResults(results);
+    /*Clearing  the input field */
+    document.querySelector('#search-term').value = '';
+
+  } else {
+    showAlert('Please enter your search term !', 'alert-error')
   }
-  displaySearchResults(results);
-  /*Clearing  the input field */
-  document.querySelector('#search-term').value = '';
-
- }else{
-   showAlert('Please enter your search term !','alert-error')
- }
 }
 
 /*displaying the results of a search */
@@ -288,41 +288,33 @@ function displaySearchResults(results) {
   document.querySelector('#search-results-heading').innerHTML = '';
   document.querySelector('#pagination').innerHTML = '';
 
-  
+
 
   results.forEach((result) => {
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
           <a href="${state.searchObj.type}-details.html?id=${result.id}">
-            ${
-              result.poster_path
-                ? `<img
+            ${result.poster_path
+        ? `<img
               src="https://image.tmdb.org/t/p/w500${result.poster_path}"
               class="card-img-top"
-              alt="${
-                state.searchObj.type === 'movie' ? result.title : result.name
-              }"
+              alt="${state.searchObj.type === 'movie' ? result.title : result.name
+        }"
             />`
-                : `<img
+        : `<img
             src="../images/no-image.png"
             class="card-img-top"
-             alt="${
-              state.searchObj.type === 'movie' ? result.title : result.name
-             }"
+             alt="${state.searchObj.type === 'movie' ? result.title : result.name
+        }"
           />`
-            }
+      }
           </a>
           <div class="card-body">
-            <h5 class="card-title">${
-              state.searchObj.type === 'movie' ? result.title : result.name
-            }</h5>
+            <h5 class="card-title">${state.searchObj.type === 'movie' ? result.title : result.name
+      }</h5>
             <p class="card-text">
-              <small class="text-muted">Release: ${
-                state.searchObj.type === 'movie'
-                  ? result.release_date
-                  : result.first_air_date
-              }</small>
+            <small class="text-muted">⭐${result.vote_average.toFixed(1)}/10</small>              
             </p>
           </div>
         `;
@@ -377,23 +369,23 @@ function displayPagination() {
 
 /*Searching and fetching data from  API */
 async function searchAPI() {
-  
-   const result = await fetch(`${state.api.APIURL}search/${state.searchObj.type}?api_key=${state.api.APIKey}&language=en-US&query=${state.searchObj.term}&page=${state.searchObj.page}`);
-   showSpinner();
-   data = result.json();
-   hideSpinner();
-   return data;
- }
+
+  const result = await fetch(`${state.api.APIURL}search/${state.searchObj.type}?api_key=${state.api.APIKey}&language=en-US&query=${state.searchObj.term}&page=${state.searchObj.page}`);
+  showSpinner();
+  data = result.json();
+  hideSpinner();
+  return data;
+}
 
 /*Showing custome message in a custom alert box*/
-function showAlert(message,className){
+function showAlert(message, className) {
   const alert = document.createElement('div');
-  alert.classList.add('alert',className);
+  alert.classList.add('alert', className);
   alert.appendChild(document.createTextNode(message));
   document.querySelector('#alert').appendChild(alert);
-  setTimeout(()=>{
+  setTimeout(() => {
     alert.remove();
-  },2500)
+  }, 2500)
 }
 function initSwiper() {
   const swiper = new Swiper('.swiper', {
@@ -402,7 +394,7 @@ function initSwiper() {
     freeMode: true,
     loop: false,
     autoplay: {
-      delay: 2500,
+      delay: 4000,
       disableOnInteraction: false,
     },
     breakpoints: {
